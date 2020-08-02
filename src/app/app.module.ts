195c13664e0fe1ce4f8aa728a6bc2f978ab3ee07
Harsh Chauhan <harsh.chd95@gmail.com>
2020-08-02 12:43:51 +0530

    pushing all files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MovieListDashboardComponent } from './movie-list-dashboard/movie-list-dashboard.component';
+import { DashboardHeaderComponent } from './header/dashboard-header/dashboard-header.component';
+import { DashboardFooterComponent } from './footer/dashboard-footer/dashboard-footer.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListDashboardComponent,
+    DashboardHeaderComponent,
+    DashboardFooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/movie-list-dashboard/movie-list-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-list-dashboard',
+  templateUrl: './movie-list-dashboard.component.html',
+  styleUrls: ['./movie-list-dashboard.component.scss']
+})
+export class MovieListDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
